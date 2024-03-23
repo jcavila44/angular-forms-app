a813e2258d13cafa9567b6099559d3eea21b70b9
Jose Carlos <josecarlos.avilaperea@gmail.com>
2024-03-22 20:44:10 -0500

    fat: updated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideMenuComponent
+  ],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    RouterModule
+  ],
+  exports: [
+    SideMenuComponent
   ]
 })
 export class SharedModule { }
